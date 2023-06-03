1c5ad919e2ed6f9be32c4f46a92ee6a9b2789607
dennyS <denismiskovsky@gmail.com>
2023-06-04 01:21:27 +0200

    Added render settings

---
+++ ZaverecnyProjekt/RenderSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace ZaverecnyProjekt
+{
+    public class RenderSettings
+    {
+        public int CellSize { get; set; } = 50;
+        public int WallWidth { get; set; } = 5;
+        public Color BackgroundColour { get; set; } = Color.FromArgb(255, 242, 242, 242);
+        public Color WallColour { get; set; } = Colors.Black;
+        public bool FillBackGround { get; set; }
+    }
+}

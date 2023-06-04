05ff60a88ccdb9b7368b7bbc6acfb32476b37601
dennyS <denismiskovsky@gmail.com>
2023-06-04 13:23:30 +0200

    Removed unnecessary usings.

--- ZaverecnyProjekt/Direction.cs
+++ ZaverecnyProjekt/Direction.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZaverecnyProjekt
 {
-        [Flags]
+    [Flags]
         public enum Direction
         {
             None = 1 << 0,

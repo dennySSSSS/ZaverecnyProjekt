6556fbfb5242729c07bf7bfa213daeca9a7be41b
dennyS <denismiskovsky@gmail.com>
2023-06-04 13:18:41 +0200

    Generate() as a param

--- ZaverecnyProjekt/MainWindow.xaml.cs
+++ ZaverecnyProjekt/MainWindow.xaml.cs
@@ -5,33 +5,28 @@ namespace ZaverecnyProjekt
     public partial class MainWindow : Window
     {
         private const Direction AllDirections = Direction.E | Direction.S | Direction.W | Direction.N;
-        Direction[,] grid = new Direction[5, 5]
-        {
-            { AllDirections, AllDirections, AllDirections, AllDirections, AllDirections },
-            { AllDirections, AllDirections, AllDirections, AllDirections, AllDirections },
-            { AllDirections, AllDirections, AllDirections, AllDirections, AllDirections },
-            { AllDirections, AllDirections, AllDirections, AllDirections, AllDirections },
-            { AllDirections, AllDirections, AllDirections, AllDirections, AllDirections }
-        };
+
+
+        
 
         public MainWindow()
         {
             InitializeComponent();
-
-            MazeGenerator mazeGenerator = new MazeGenerator();
-            mazeGenerator.Generate(grid);
-
-
-            MazeRenderer renderer = new MazeRenderer(myCanvas);
-            RenderSettings renderSettings = new RenderSettings();
-            renderer.DrawMaze(grid, renderSettings);
+            
 
         }
 
         private void mainButton_Click(object sender, RoutedEventArgs e)
         {
-            //drawArray();
+            myCanvas.Children.Clear();
 
+            MazeGenerator mazeGenerator = new MazeGenerator();
+            //mazeGenerator.Generate();
+
+
+            MazeRenderer renderer = new MazeRenderer(myCanvas);
+            RenderSettings renderSettings = new RenderSettings();
+            renderer.DrawMaze(mazeGenerator.Generate(5, 10), renderSettings);
 
         }
     }

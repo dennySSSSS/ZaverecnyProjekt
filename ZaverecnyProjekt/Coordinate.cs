6e793197512eee5cb0f94e771aa43c11e620f628
dennyS <denismiskovsky@gmail.com>
2023-06-04 13:24:46 +0200

    Removed unnecessary usings.

--- ZaverecnyProjekt/Coordinate.cs
+++ ZaverecnyProjekt/Coordinate.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ZaverecnyProjekt
+﻿namespace ZaverecnyProjekt
 {
-        public struct Coordinate
+    public struct Coordinate
         {
             public int X { get; set; }
             public int Y { get; set; }

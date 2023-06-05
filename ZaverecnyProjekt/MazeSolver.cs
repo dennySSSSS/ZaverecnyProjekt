cf1018918ed93f89189cab7ad43a5ddd413b8396
Denis Miškovský <miskode@student.uzlabina.cz>
2023-06-05 16:27:20 +0200

    Stack experiment

--- ZaverecnyProjekt/MazeSolver.cs
+++ ZaverecnyProjekt/MazeSolver.cs
@@ -10,7 +10,43 @@ namespace ZaverecnyProjekt
     internal class MazeSolver
     {
         Stack<Coordinate> stack = new Stack<Coordinate>();
+        Direction[,] grid;
+        RenderSettings renderSettings;
 
+        public MazeSolver(Direction[,] grid, RenderSettings renderSettings)
+        {
+            this.grid = grid;
+            this.renderSettings = renderSettings;
+        }
+
+        public Stack<Coordinate> ShowPath()
+        {
+            stack.Push(new Coordinate(0, 0));
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (!grid[i, j].HasFlag(Direction.S))
+                    {
+                        stack.Push(new Coordinate(i++, j));
+                    }
+                    if (!grid[i, j].HasFlag(Direction.E))
+                    {
+                        stack.Push(new Coordinate(i, j++));
+                    }
+                    if(!grid[i, j].HasFlag(Direction.N))
+                    {
+                        stack.Push(new Coordinate(i--, j));
+                    }
+                    if (!grid[i, j].HasFlag(Direction.W))
+                    {
+                        stack.Push(new Coordinate(i, j--));
+                    }
+                }
+            }
+            return stack;
+        }
 
     }
 }

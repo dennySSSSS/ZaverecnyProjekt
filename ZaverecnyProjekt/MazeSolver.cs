60542f257e3f48211657641a37a13cc9d469d7f3
Denis Miškovský <miskode@student.uzlabina.cz>
2023-06-05 15:49:17 +0200

    Created maze solver class.

---
+++ ZaverecnyProjekt/MazeSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaverecnyProjekt
+{
+    internal class MazeSolver
+    {
+        Stack<Coordinate> stack = new Stack<Coordinate>();
+
+
+    }
+}
